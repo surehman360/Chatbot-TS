b2997d632bd73750cb120f888fec3c7d9a78f79e
unknown <salmanurrehman360@gmail.com>
2023-11-27 20:50:01 +0500

    secured

--- src/controllers/openaiController.ts
+++ src/controllers/openaiController.ts
@@ -1,7 +1,21 @@
 // controllers/openaiController.ts
 import OpenAI from "openai";
 
-const openai = new OpenAI({ apiKey: "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" });
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Retrieve the API key from the environment variables
+const apiKey = process.env.OPENAI_API_KEY;
+
+if (!apiKey) {
+    console.error('Error: OPENAI_API_KEY not found in the environment variables.');
+    process.exit(1); // Exit the process if the API key is not found
+}
+
+const openai = new OpenAI({ apiKey });
+
 
 async function chatToAndFro(messages: any) {
   try {
